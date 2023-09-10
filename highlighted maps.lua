e683fb3d5ccfd675ba7b7aca52edda1be32a481a
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-09-10 21:27:23 +0100

    Update highlighted maps.lua

--- highlighted maps.lua
+++ highlighted maps.lua
@@ -1,8 +1,12 @@
 return {
   3,
-  "N13",
+  "HEWWO1A_1_:3_A_HEWWO_V1",
   "POOPY",
-  "The Overflow",
+  "Agitato",
+  "Travel 1.5 : Restart Time",
+  "Dark drive",
+  "Familiar Ruins 2023 Finale Update",
+  "Mandragora 2.0"
   "(Unnerfed) Technological Destruction",
   "Wall Jump Practice",
   "Agitato 3.0 OLD",
