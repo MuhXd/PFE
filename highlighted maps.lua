b844415a58080009903bca4e16e23676b1ab8c25
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-09-11 16:02:34 +0100

    Update highlighted maps.lua

--- highlighted maps.lua
+++ highlighted maps.lua
@@ -1,19 +1,18 @@
 return {
-  3,
-  "HEWWO1A_1_:3_A_HEWWO_V2",
+  2,
+  "HEWWO1A_1_:3_A_HEWWO_V3",
   "Lingering Enterprise 2.0",
-  "Octane",
   "Holy Knight - BlackMagik [Old]",
   "Dark drive",
   "Familiar Ruins 2023 Finale Update",
   "Mandragora 2.0",
   "Digital Rooms",
-  "Electro Sound",
   "Familiar Ruins 2023 Finale Update",
   "Holy Knight",
   "Illegal Failure",
   "Insane Mashup [Old Layout] [May 8, 2021]",
   "Insane Mashup",
+  "POOPIS",
   ">w<",
   ":3"
 }
